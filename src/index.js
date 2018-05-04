a4b501afc2b14e3b452273357b5bb3b74f592ff0
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-05-04 16:14:29 -0400

    Looks sexy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const map = new mapboxgl.Map({
     container: "map",
     center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
     zoom: 12, // starting zoom
-    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+    style: "mapbox://styles/tito4gsus/cjgsbkys3001e2rqh2uxz0eto" // mapbox has lots of different map styles available.
   });
 
 const marker = buildMarker("activities", [-74.009151, 40.705086]).addTo(map);
